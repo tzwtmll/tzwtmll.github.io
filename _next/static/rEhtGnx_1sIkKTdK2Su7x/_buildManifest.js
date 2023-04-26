81223b8f95b41479750650b249498038edec97ff
tzwtmll <104382132+tzwtmll@users.noreply.github.com>
2023-04-26 10:11:45 +0000

    Deploying to gh-pages from @ tzwtmll/tzwtmll.github.io@73fb003879d8184e800c823a77f2f7f162fa5bd3 🚀

---
+++ _next/static/rEhtGnx_1sIkKTdK2Su7x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/21ceccf7ecbc3d84.css","static/chunks/pages/index-ab84add41b16adc2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
