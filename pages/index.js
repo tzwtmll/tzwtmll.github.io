d172efa11906acd53f4c846b1ef43fd527bac552
pdd <17683992872@163.com>
2023-04-27 14:07:11 +0800

    123

--- pages/index.js
+++ pages/index.js
@@ -92,16 +92,17 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="https://tzwtmll.github.io/v2docs"
+            href="https://vercel.com/new/templates"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs 2.0 <span>-&gt;</span>
+              Templates <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              更加简洁的二代文档&nbsp;正在开发中.
+              Discover and deploy boilerplate example Next.js
+              projects.&nbsp;projects.
             </p>
           </a>
         </div>
