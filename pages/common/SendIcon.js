f7d6c252743d0bbc3f5ac72e541652c77428fc04
pdd <17683992872@163.com>
2023-05-08 17:41:15 +0800

    🐞 fix: calss错误

--- pages/common/SendIcon.js
+++ pages/common/SendIcon.js
@@ -27,7 +27,7 @@ export const SendIcon = ({
     //   </svg>
     <svg
       t="1683533164107"
-      class="icon"
+      className="icon"
       viewBox="0 0 1024 1024"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
